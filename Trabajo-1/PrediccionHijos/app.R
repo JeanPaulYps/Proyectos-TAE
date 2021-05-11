5c826e007bf624ba9c2cd128c390073c53ff2660
Jean Paul <yjeanpaul@gmail.com>
2021-05-11 17:58:22 -0500

    Graficos de los tickets de entradas

--- Trabajo-1/PrediccionHijos/app.R
+++ Trabajo-1/PrediccionHijos/app.R
@@ -14,63 +14,69 @@ library(psych)
 load("Variables.Rdata")
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
-
-    # Application title
-    titlePanel("Prediccion de hijos con base a las caracteristicas de un hogar"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
+ui <- navbarPage("Prediccion hijos",
+                    tabPanel("Bienvenida ",
+                             h1("Bienvenido a la aplicacion"),
+                              p("En esta pagina web se presenta un modelo estadístico basado en la encuesta de 
+                                satisfaccion del vida del DANE del año 2020. Este modelo te permite seleccionar
+                                las características de un hogar como ... Esto te permite obtener una prediccion
+                                de cuantos hijos existiran en un hogar asi. Explora las posibilidades que tienes
+                                cambiando las opciones")),
           
-          selectInput("Educacion_jefe", 
-                      label = "Selecciona el nivel de educacion del jefe(a) del hogar ",
-                      choices = c("Basica Primaria (1 - 5)",
-                                  "Basica secundaria (6 - 9)",
-                                  "Media (10 - 13)",
-                                  "Ninguno",
-                                  "Postgrado con titulo",
-                                  "Postgrado sin titulo",
-                                  "Tecnico con titulo",
-                                  "Tecnico sin titulo",
-                                  "Tecnologico con titulo",
-                                  "Tecnologico sin titulo",
-                                  "Universitario con titulo",
-                                  "Universitario sin titulo"
-                                  ),
-                      selected = "Basica Primaria (1 - 5)"),
-          
-          selectInput("Estado_civil_jefe", 
-                      label = "Selecciona el estado civil del jefe(a) ",
-                      choices = c("Esta casado(a)",
-                                  "Esta separado(a) o divorciado(a)",
-                                  "Esta soltero(a)",
-                                  "Esta viudo(a)",
-                                  "No esta casado(a) y vive en pareja hace dos anios o mas",
-                                  "No esta casado(a) y vive en pareja hace menos de dos anios"
-                      ),
-                      selected = "Esta casado(a)"),
-          
-          sliderInput("Grado_satisfaccion_jefe",
-                      "Grado de satisfaccion del jefe:",
-                      min = 0,
-                      max = 10,
-                      value = 0),
-        ),
-        
-      
-        
- 
-        
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           h2("Predicciones del modelo"),
-           p(textOutput("descripcionVariables")),
-           strong(textOutput("textoResultado"))
-        )
-        
-    )
+                   tabPanel("Aplicacion", 
+                            sidebarLayout(
+                              sidebarPanel(
+                                
+                                selectInput("Educacion_jefe", 
+                                            label = "Selecciona el nivel de educacion del jefe(a) del hogar ",
+                                            choices = c("Basica Primaria (1 - 5)",
+                                                        "Basica secundaria (6 - 9)",
+                                                        "Media (10 - 13)",
+                                                        "Ninguno",
+                                                        "Postgrado con titulo",
+                                                        "Postgrado sin titulo",
+                                                        "Tecnico con titulo",
+                                                        "Tecnico sin titulo",
+                                                        "Tecnologico con titulo",
+                                                        "Tecnologico sin titulo",
+                                                        "Universitario con titulo",
+                                                        "Universitario sin titulo"
+                                            ),
+                                            selected = "Basica Primaria (1 - 5)"),
+                                
+                                selectInput("Estado_civil_jefe", 
+                                            label = "Selecciona el estado civil del jefe(a) ",
+                                            choices = c("Esta casado(a)",
+                                                        "Esta separado(a) o divorciado(a)",
+                                                        "Esta soltero(a)",
+                                                        "Esta viudo(a)",
+                                                        "No esta casado(a) y vive en pareja hace dos anios o mas",
+                                                        "No esta casado(a) y vive en pareja hace menos de dos anios"
+                                            ),
+                                            selected = "Esta casado(a)"),
+                                
+                                sliderInput("Grado_satisfaccion_jefe",
+                                            "Grado de satisfaccion del jefe:",
+                                            min = 0,
+                                            max = 10,
+                                            value = 0),
+                              ),
+                              
+                              
+                              
+                              
+                              
+                              
+                              # Show a plot of the generated distribution
+                              mainPanel(
+                                h2("Predicciones del modelo"),
+                                p(textOutput("descripcionVariables")),
+                                strong(textOutput("textoResultado"))
+                              )
+                              
+                            )
+                        )
+    
     
 )
 
